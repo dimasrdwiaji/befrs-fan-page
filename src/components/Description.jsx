97eb3032e2cf7e8448e3a36933e5f6f1b4c418f5
dimasrdwiaji <79129785+dimasrdwiaji@users.noreply.github.com>
2022-09-03 19:09:25 +0700

    description revition. need to make better layout

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -3,20 +3,18 @@ import { Card } from "flowbite-react";
 
 export default function Description({ desc }) {
   return (
-    <div className="my-8 flex justify-center w-screen">
-      <Card
-        horizontal={true}
-        imgSrc={desc[0].snippet.thumbnails.high.url}
-        imgAlt={desc[0].snippet.title}
-        className='w-screen'
-      >
-        <h5 className="text-2xl font-bold tracking-wide text-gray-900 dark:text-white">
-          {desc[0].snippet.title}
-        </h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          {desc[0].snippet.description}
-        </p>
-      </Card>
+    <div className="my-8 flex flex-col justify-center w-screen">
+      <h1>About Us</h1>
+      <hr class="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700"></hr>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        {desc[0].snippet.description}
+      </p>
+      <figure>
+        <img
+          src={desc[0].snippet.thumbnails.high.url}
+          alt={desc[0].snippet.title}
+        />
+      </figure>
     </div>
   );
 }

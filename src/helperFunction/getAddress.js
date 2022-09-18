8cf290efd08e2f12b323e6b9a3ddb1f27815c802
dimasrdwiaji <79129785+dimasrdwiaji@users.noreply.github.com>
2022-09-18 18:26:08 +0700

    extract address done and assigned to videoArray

--- src/helperFunction/getAddress.js
+++ src/helperFunction/getAddress.js
@@ -1,56 +1,36 @@
-const fs = require('fs')
+const fs = require("fs");
 
-let jsonString
+let jsonString;
 let videoArray;
-let firstSplit = [];
-let containAddress = [];
-let address = [];
-let combinedAddress = [];
 
 try {
-    jsonString = fs.readFileSync('../assets/fetched_data/uploadedVideos.json', 'utf-8');
-    videoArray = JSON.parse(jsonString)   
+  jsonString = fs.readFileSync(
+    "../assets/fetched_data/uploadedVideos.json",
+    "utf-8"
+  );
+  videoArray = JSON.parse(jsonString);
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
 
-// Split by address
-firstSplit = videoArray.map((video) => (
-    video.snippet.description.split('ADDRESS: ')
+function uniqueAddress(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
+let obtainAddress = (array) =>
+  array
+    .map((item) =>
+      item.snippet.description
+        .split(/\r?\n/)
+        .filter((l) => l.indexOf("ADDRESS:") === 0)
+        .filter(uniqueAddress)
+    )
+    .map((l) => l.map((i) => i.replace("ADDRESS: ", "")));
+
+let obtainedAddress = obtainAddress(videoArray);
+
+videoArray.map((video) => (
+  Object.assign(video.snippet, {address: obtainedAddress[videoArray.indexOf(video)]})
 ))
 
-
-// Delete description that has no address
-Object.keys(firstSplit).forEach(key => {
-    if(firstSplit[key] === undefined) {
-        delete firstSplit[key];
-    }
-});
-
-// Remove noise
-containAddress = firstSplit.map((element) => (
-    element.slice(1)
-))
-
-// Extract address
-address = containAddress.map((item) => (
-    item.map((arr) => (
-        arr.split('\n')[0]
-    ))
-))
-
-// Remove empty array
-address = address.filter(item => {
-    return item != null && item!= '';
-})
-
-// Combine address to an array
-combinedAddress = Object.values(address).flat()
-
-// Remove duplicate address
-const uniqueAddress = [...new Set(combinedAddress)]
-
-// Convert set to array
-const uniqueAddressArr = Array.from(uniqueAddress)
-
-export default uniqueAddressArr;
+export const videoList = videoArray;
